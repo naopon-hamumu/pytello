364ac6e87ad31f97c602a0a57d0d6fa2a735e500
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-08-20 21:30:30 +0900

    ドローンからのレスポンスを受信する

--- drone_manager.py
+++ drone_manager.py
@@ -11,64 +11,64 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 class DroneManager(object):
-  def __init__(self, host_ip='192.168.10.2', host_port=8889,
-               drone_ip='192.168.10.1', drone_port=8889):
-    self.host_ip = host_ip
-    self.host_port = host_port
-    self.drone_ip = drone_ip
-    self.drone_port = drone_port
-    self.drone_address = (drone_ip, drone_port)
-    # ソケットの作成
-    # アドレスファミリー => AF_INET：IPv4インターネットプロトコル
-    # ソケットタイプ => SOCK_DGRAM：UDPソケット通信で利用される
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # ローカルのアドレスにバインド
-    self.socket.bind((self.host_ip, self.host_port))
+    def __init__(self, host_ip='192.168.10.2', host_port=8889,
+                drone_ip='192.168.10.1', drone_port=8889):
+        self.host_ip = host_ip
+        self.host_port = host_port
+        self.drone_ip = drone_ip
+        self.drone_port = drone_port
+        self.drone_address = (drone_ip, drone_port)
+        # ソケットの作成
+        # アドレスファミリー => AF_INET：IPv4インターネットプロトコル
+        # ソケットタイプ => SOCK_DGRAM：UDPソケット通信で利用される
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # ローカルのアドレスにバインド
+        self.socket.bind((self.host_ip, self.host_port))
 
-    self.response = None
-    self.stop_event = threading.Event()
-    # タプル型：要素の順番を変えず、データ型が固定の場合に使用される
-    response_thread = threading.Thread(target=self.receive_response,
-                                       args=(self.stop_event, ))
-    self._response_thread.start()
+        self.response = None
+        self.stop_event = threading.Event()
+        # タプル型：要素の順番を変えず、データ型が固定の場合に使用される
+        self._response_thread = threading.Thread(target=self.receive_response,
+                                        args=(self.stop_event, ))
+        self._response_thread.start()
 
-    self.send_command('command')
-    self.send_command('streamon')
+        self.send_command('command')
+        self.send_command('streamon')
 
-  def receive_response(self, stop_event):
-    while not stop_event.is_set():
-      try:
-        self.response, ip = self.socket.recvfrom(3000)
-        logger.info({'action': 'receive_response',
-                    'response': self.response})
-      except socket.error as ex:
-        logger.error({'action': 'receive_response',
-                     'ex': ex})
-        break
+    def receive_response(self, stop_event):
+        while not stop_event.is_set():
+            try:
+                self.response, ip = self.socket.recvfrom(3000)
+                logger.info({'action': 'receive_response',
+                            'response': self.response})
+            except socket.error as ex:
+                logger.error({'action': 'receive_response',
+                            'ex': ex})
+                break
 
-  def __dell__(self):
-    self.stop()
+    def __dell__(self):
+        self.stop()
 
-  def stop(self):
-    self.stop_event.set()
-    # ソケットのクローズ&ファイルディスクリプタの削除
-    self.socket.close()
+    def stop(self):
+        self.stop_event.set()
+        # ソケットのクローズ&ファイルディスクリプタの削除
+        self.socket.close()
 
-  def send_command(self, command):
-    logger.info({'action': 'send_command', 'command': command})
-    self.socket.sendto(command.encode('utf-8'), self.drone_address)
+    def send_command(self, command):
+        logger.info({'action': 'send_command', 'command': command})
+        self.socket.sendto(command.encode('utf-8'), self.drone_address)
 
-  def takeoff(self):
-    self.send_command('takeoff')
+    def takeoff(self):
+        self.send_command('takeoff')
 
-  def land(self):
-    self.send_command('land')
+    def land(self):
+        self.send_command('land')
 
 if __name__ == '__main__':
-  drone_manager = DroneManager()
-  drone_manager.takeoff()
+    drone_manager = DroneManager()
+    drone_manager.takeoff()
 
-  time.sleep(10)
+    time.sleep(10)
 
-  drone_manager.land()
-  drone_manager.stop()
+    drone_manager.land()
+    drone_manager.stop()
--- tools/image_file_detect.py
+++ tools/image_file_detect.py
@@ -3,19 +3,19 @@ import cv2 as cv
 face_cascade = cv.CascadeClassifier('./tools/haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('./tools/haarcascade_eye.xml')
 
-img = cv.imread('./tools/shori.jpg')
+img = cv.imread('')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 print(len(faces))
 
 for (x, y, w, h) in faces:
-  cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-  eye_gray = gray[y:y+h, x:x+w]
-  eye_color = img[y:y+h, x:x+w]
-  eyes = eye_cascade.detectMultiScale(eye_gray)
-  for (ex, ey, ew, eh) in eyes:
-    cv.rectangle(eye_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    eye_gray = gray[y:y+h, x:x+w]
+    eye_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(eye_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv.rectangle(eye_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
 cv.imshow('img', img)
 cv.waitKey(0)
