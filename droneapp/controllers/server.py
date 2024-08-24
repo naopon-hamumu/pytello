bd900663b1cb0fa2dff656a5bc4a884360a757b4
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-08-24 21:41:07 +0900

    POSTでバックエンドにコマンドを送信する

--- droneapp/controllers/server.py
+++ droneapp/controllers/server.py
@@ -1,5 +1,8 @@
 import logging
+
+from flask import jsonify
 from flask import render_template
+from flask import request
 import config
 
 logger = logging.getLogger(__name__)
@@ -12,6 +15,12 @@ def index():
 @app.route('/controllers/')
 def controller():
     return render_template('controller.html')
+
+@app.route('/api/command/', methods=['POST'])
+def command():
+    cmd = request.form.get('command')
+    logger.info({'action': 'command', 'cmd': cmd})
+    return jsonify(status='success'), 200
 
 def run():
     app.run(host=config.WEB_ADDRESS, port=config.WEB_PORT, threaded=True)
