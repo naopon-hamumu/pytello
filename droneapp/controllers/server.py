45063bc20645f8f78084146eba4dc7c713e894bf
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-08-24 21:16:54 +0900

    LayoutをExtendsする

--- droneapp/controllers/server.py
+++ droneapp/controllers/server.py
@@ -9,5 +9,9 @@ app = config.app
 def index():
     return render_template('index.html')
 
+@app.route('/controllers/')
+def controller():
+    return render_template('controller.html')
+
 def run():
     app.run(host=config.WEB_ADDRESS, port=config.WEB_PORT, threaded=True)
